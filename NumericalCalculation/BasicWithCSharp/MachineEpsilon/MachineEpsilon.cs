551546ca298e78ddaf5544c93f6766680d284cef
MasuqaT <occar@hotmail.co.jp>
2016-09-27 21:00:29 +0900

    Fix mistake of the definition

--- NumericalCalculation/BasicWithCSharp/MachineEpsilon/MachineEpsilon.cs
+++ NumericalCalculation/BasicWithCSharp/MachineEpsilon/MachineEpsilon.cs
@@ -17,55 +17,49 @@ public class MachineEpsilon
 
 	static void CalcFloatEpsilon()
 	{
-		float a = 1.0f;
-		float b = a + 1.0f;
+		float eps = 1.0f;
 
-		while (b > 1.0f)
+		while (1.0f + eps / 2.0f > 1.0f)
 		{
-			a *= 0.5f;
-			b = 1.0f + a;
+			eps /= 2.0f;
 
-			Console.WriteLine($"{b:e30}");
+			Console.WriteLine($"{(1.0f + eps):e30}");
 		}
 
-		Console.WriteLine($"1+ϵ : {(1.0f + a):e30}");
-		Console.WriteLine($"1+2ϵ: {(1.0f + a * 2.0f):e30}");
-		Console.WriteLine($"float machine epsilon: {a:e30}");
+		Console.WriteLine($"1+eps  : {(1.0f + eps):e30}");
+		Console.WriteLine($"1+eps/2: {(1.0f + eps / 2.0f):e30}");
+		Console.WriteLine($"float machine epsilon: {eps:e30}");
 	}
 
 	static void CalcDoubleEpsilon()
 	{
-		double a = 1.0;
-		double b = a + 1.0;
+		double eps = 1.0;
 
-		while (b > 1.0)
+		while (1.0 + eps / 2.0 > 1.0)
 		{
-			a *= 0.5;
-			b = 1.0 + a;
+			eps /= 2.0;
 
-			Console.WriteLine($"{b:e30}");
+			Console.WriteLine($"{(1.0 + eps):e30}");
 		}
 
-		Console.WriteLine($"1+ϵ : {(1.0 + a):e30}");
-		Console.WriteLine($"1+2ϵ: {(1.0 + a * 2.0):e30}");
-		Console.WriteLine($"double machine epsilon: {a:e30}");
+		Console.WriteLine($"1+eps  : {(1.0 + eps):e30}");
+		Console.WriteLine($"1+eps/2: {(1.0 + eps / 2.0):e30}");
+		Console.WriteLine($"double machine epsilon: {eps:e30}");
 	}
 
 	static void CalcDecimalEpsilon()
 	{
-		decimal a = 1.0m;
-		decimal b = a + 1.0m;
+		decimal eps = 1.0m;
 
-		while (b > 1.0m)
+		while (1.0m + eps / 2.0m > 1.0m)
 		{
-			a *= 0.5m;
-			b = 1.0m + a;
+			eps /= 2.0m;
 
-			Console.WriteLine($"{b:e30}");
+			Console.WriteLine($"{(1.0m + eps):e30}");
 		}
 
-		Console.WriteLine($"1+ϵ : {(1.0m + a):e30}");
-		Console.WriteLine($"1+2ϵ: {(1.0m + a * 2.0m):e30}");
-		Console.WriteLine($"decimal machine epsilon: {a:e30}");
+		Console.WriteLine($"1+eps  : {(1.0m + eps):e30}");
+		Console.WriteLine($"1+eps/2: {(1.0m + eps / 2.0m):e30}");
+		Console.WriteLine($"decimal machine epsilon: {eps:e30}");
 	}
 }
