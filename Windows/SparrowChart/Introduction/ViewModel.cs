8634f3c4b4155863e249ef3a57f56b7c5e5214e2
MasuqaT <occar@hotmail.co.jp>
2016-10-04 20:24:56 +0900

    Add sparrowchart introduce

---
+++ Windows/SparrowChart/Introduction/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Introduction
+{
+	public class ViewModel
+	{
+		public IEnumerable<Point> Points { get; } = Enumerable.Range(0, 10 + 1)
+			.Select(x => new Point { X = x, Fx = Math.Pow(x, 0.5) });
+	}
+
+	public struct Point
+	{
+		public double X { get; set; }
+		public double Fx { get; set; }
+	}
+}
