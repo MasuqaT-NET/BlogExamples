fb337947456bfac0f85595a18d696a386dbce9d3
MasuqaT <occar@hotmail.co.jp>
2016-10-08 23:54:31 +0900

    Added ReactiveUI vs ReactiveProperty example

---
+++ Windows/ReactiveFrameworks/ByReactiveProperty/ReactivePropertyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ByReactiveProperty
+{
+	class ReactivePropertyViewModel
+	{
+		public ReactiveProperty<string> Text { get; } = new ReactiveProperty<string>("InitValue");
+
+		public ReactiveProperty<string> LabelText { get; }
+
+		public ReactiveProperty<string> ButtonText { get; }
+
+		public ReactiveCommand ButtonCommand { get; }
+
+		public ReactiveCollection<string> LogList { get; } = new ReactiveCollection<string> { "First" };
+
+		public ReactivePropertyViewModel()
+		{
+			LabelText = Text.Select(x => string.Concat(x.ToUpper().Reverse())).ToReactiveProperty();
+			ButtonText = Text.Throttle(TimeSpan.FromSeconds(2.0)).ToReactiveProperty("Hello");
+
+			ButtonCommand = Text.Select(x => x.Length > 10).ToReactiveCommand();
+			ButtonCommand.Subscribe(_ => LogList.Add($"Button pressed. @{Text.Value}"));
+		}
+	}
+}
---
+++ Windows/ReactiveFrameworks/ByReactiveUI/ReactiveUIViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ReactiveUI;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ByReactiveUI
+{
+	class ReactiveUIViewModel : ReactiveObject
+	{
+		private string text = "InitValue";
+		public string Text
+		{
+			get
+			{
+				return text;
+			}
+			set
+			{
+				this.RaiseAndSetIfChanged(ref text, value);
+			}
+		}
+
+		public ObservableAsPropertyHelper<string> LabelText { get; }
+
+		public ObservableAsPropertyHelper<string> ButtonText { get; }
+
+		public ReactiveCommand<object> ButtonCommand { get; }
+
+		public ReactiveList<string> LogList { get; } = new ReactiveList<string> { "First" };
+
+		public ReactiveUIViewModel()
+		{
+			var textObservable = this.WhenAnyValue(x => x.Text);
+			LabelText = new ObservableAsPropertyHelper<string>(textObservable.Select(x => string.Concat(x.ToUpper().Reverse())), x => this.RaisePropertyChanged(nameof(LabelText)), x => { });
+			ButtonText = new ObservableAsPropertyHelper<string>(textObservable.Throttle(TimeSpan.FromSeconds(2.0)), x => this.RaisePropertyChanged(nameof(ButtonText)), "Hello");
+
+			ButtonCommand = ReactiveCommand.Create(this.WhenAny(x => x.Text, x => x.Value.Length > 10));
+			ButtonCommand.Subscribe(_ => LogList.Add($"Button pressed. @{Text}"));
+		}
+	}
+}
