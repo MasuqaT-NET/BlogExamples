c67bb62f8d8b1633b4ad0333ef6013417ecd786e
MasuqaT <occar@hotmail.co.jp>
2016-10-09 16:48:40 +0900

    Add "SparrowChart customize legend" example

---
+++ Windows/SparrowChart/CustomizeLegend/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomizeLegend
+{
+	class ViewModel
+	{
+		public IEnumerable<Point> SqrtPoints { get; } = Enumerable.Range(0, 10 + 1)
+			.Select(x => new Point { X = x, Fx = Math.Pow(x, 0.5) });
+		public IEnumerable<Point> LnPoints { get; } = Enumerable.Range(1, 10)
+			.Select(x => new Point { X = x, Fx = Math.Log(x) });
+		public IEnumerable<Point> LinePoints { get; } = Enumerable.Range(0, 10 + 1)
+			.Select(x => new Point { X = x, Fx = x / 2.0 - 1 });
+	}
+
+	public class Point
+	{
+		public double X { get; set; }
+		public double Fx { get; set; }
+	}
+}
