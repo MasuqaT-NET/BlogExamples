12c5233f56a8da595deea8708ab92f5182cd26f4
MasuqaT <occar@hotmail.co.jp>
2016-10-16 20:23:36 +0900

    Added Razor comparison example

---
+++ Web/AspNet/RazorComparison/Legacy/LegacyRazor.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using System.Web.Razor;
+using System.Web.Razor.Generator;
+
+namespace Legacy
+{
+	class LegacyRazor
+	{
+		static void Main(string[] args)
+		{
+			Run();
+		}
+
+		static void Run()
+		{
+			// ホスト作成
+			var language = new CSharpRazorCodeLanguage();
+			var host = new RazorEngineHost(language)
+			{
+				DefaultBaseClass = nameof(Legacy) + "." + nameof(LegacyTemplateBase),
+				DefaultClassName = "Generated",
+				DefaultNamespace = "RazorTemplate",
+				// 生成されるメソッド名の指定（これはデフォルトと同じものを指定している）
+				GeneratedClassContext = new GeneratedClassContext(
+					nameof(LegacyTemplateBase.Execute),
+					nameof(LegacyTemplateBase.Write),
+					nameof(LegacyTemplateBase.WriteLiteral)
+				)
+			};
+			// デフォルトで挿入する名前空間を追加
+			host.NamespaceImports.Add(nameof(System));
+
+			var engine = new RazorTemplateEngine(host);
+			using (var reader = new StreamReader(File.OpenRead("../../../Page.cshtml")))
+			{
+				// コードDOM生成
+				var generatorResults = engine.GenerateCode(reader);
+				System.CodeDom.CodeCompileUnit unit = generatorResults.GeneratedCode;
+				var provider = new CSharpCodeProvider();
+
+				// 表示用
+				provider.GenerateCodeFromCompileUnit(unit, Console.Out, new CodeGeneratorOptions());
+				Console.WriteLine("------------------------------------------------------------------------");
+
+				// 使用アセンブリ参照
+				var parameters = new CompilerParameters();
+				parameters.ReferencedAssemblies.Add("System.Core.dll");
+				parameters.ReferencedAssemblies.Add(typeof(LegacyTemplateBase).Assembly.Location);
+
+				// コンパイル
+				var compilerResults = provider.CompileAssemblyFromDom(parameters, unit);
+
+				// 実行
+				var typeName = host.DefaultNamespace + "." + host.DefaultClassName;
+				var instance = compilerResults.CompiledAssembly.CreateInstance(typeName) as LegacyTemplateBase;
+				var generatedString = instance.Generate();
+
+				// 結果表示
+				Console.Write(generatedString);
+			}
+		}
+	}
+
+	public abstract class LegacyTemplateBase
+	{
+		StringBuilder builder = new StringBuilder();
+
+		public void Write(object value)
+		{
+			builder.Append(value);
+		}
+
+		public void WriteLiteral(string literal)
+		{
+			builder.Append(literal);
+		}
+
+		public abstract void Execute();
+
+		public string Generate()
+		{
+			Execute();
+			return builder.ToString();
+		}
+	}
+}
---
+++ Web/AspNet/RazorComparison/New/NewRazor.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Razor.CodeGenerators;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New
+{
+	public class NewRazor
+	{
+		public static void Main(string[] args)
+		{
+			Run().Wait();
+		}
+
+		static async Task Run()
+		{
+			// ホスト生成
+			var language = new CSharpRazorCodeLanguage();
+			var host = new RazorEngineHost(language)
+			{
+				DefaultBaseClass = nameof(New) + "." + nameof(NewTemplateBase),
+				DefaultClassName = "Generated",
+				DefaultNamespace = "RazorTemplate",
+				// 生成されるメソッド名の指定（これはデフォルトと同じものを指定している）
+				GeneratedClassContext = new GeneratedClassContext(
+					nameof(NewTemplateBase.ExecuteAsync),
+					nameof(NewTemplateBase.Write),
+					nameof(NewTemplateBase.WriteLiteral),
+					new GeneratedTagHelperContext { }
+				)
+			};
+			// デフォルトで挿入する名前空間を追加
+			host.NamespaceImports.Add(nameof(System));
+			host.NamespaceImports.Add(nameof(System) + "." + nameof(System.Threading) + "." + nameof(System.Threading.Tasks));
+
+			var engine = new RazorTemplateEngine(host);
+			using (var reader = new StreamReader(File.OpenRead("../Page.cshtml")))
+			{
+				//ソースコード生成
+				var generatorResults = engine.GenerateCode(reader);
+				string source = generatorResults.GeneratedCode;
+
+				//表示用
+				Console.Write(source);
+				Console.WriteLine("------------------------------------------------------------------------");
+
+				var dllBase = Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location);
+				//使用アセンブリ参照
+				var references = new MetadataReference[]
+				{
+					MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location),
+					MetadataReference.CreateFromFile(dllBase + @"\mscorlib.dll"),
+					MetadataReference.CreateFromFile(dllBase + @"\System.Runtime.dll"),
+					MetadataReference.CreateFromFile(dllBase + @"\System.Linq.dll"),
+					MetadataReference.CreateFromFile(dllBase + @"\System.Threading.Tasks.dll"),
+					MetadataReference.CreateFromFile(typeof(NewTemplateBase).GetTypeInfo().Assembly.Location)
+				};
+
+				//コンパイル
+				var compilation = CSharpCompilation.Create("NewPage")
+					.WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))
+					.AddReferences(references)
+					.AddSyntaxTrees(CSharpSyntaxTree.ParseText(source));
+
+				using (var ms = new MemoryStream())
+				{
+					var result = compilation.Emit(ms);
+					ms.Seek(0, SeekOrigin.Begin);
+					var assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+
+					//実行
+					var typeName = host.DefaultNamespace + "." + host.DefaultClassName;
+					var instance = assembly.CreateInstance(typeName) as NewTemplateBase;// .GetMethod(nameof(NewTemplateBase.Execute))
+					var generatedString = await instance.GenerateAsync();
+
+					//結果表示
+					Console.Write(generatedString);
+				}
+			}
+		}
+	}
+
+	public abstract class NewTemplateBase
+	{
+		StringBuilder builder = new StringBuilder();
+
+		public void Write(object value)
+		{
+			builder.Append(value);
+		}
+
+		public void WriteLiteral(string literal)
+		{
+			builder.Append(literal);
+		}
+
+		public abstract Task ExecuteAsync();
+
+		public async Task<string> GenerateAsync()
+		{
+			await ExecuteAsync();
+			return builder.ToString();
+		}
+	}
+}
