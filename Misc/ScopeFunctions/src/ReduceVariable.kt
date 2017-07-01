1b3205baed07df750c06e8d63579bacdbd060b43
MasuqaT <occar@hotmail.co.jp>
2017-07-01 21:22:34 +0900

    Updated previous commit.

--- Misc/ScopeFunctions/src/ReduceVariable.kt
+++ Misc/ScopeFunctions/src/ReduceVariable.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     val array = arrayOf(50, 60, 70, 70, 100)
-    val variance = array.run { map { it * it }.average() - average() * average() }
+    val mean = array.run { sum() / count().toDouble() }
+    val variance = array.run { map { it * it }.run { sum() / count().toDouble() } - mean * mean }
     println(variance)
 }
