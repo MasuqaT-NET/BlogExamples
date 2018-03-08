2113f1b6829497ee50a69cd535d6fa73e68b0b74
MasuqaT <occar@hotmail.co.jp>
2018-03-08 21:13:03 +0900

    Fix errors reported by TSLint.

--- Web/React/react-typescript-toolchain-2018-spring/src/App.tsx
+++ Web/React/react-typescript-toolchain-2018-spring/src/App.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+import logo from './logo.svg';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
