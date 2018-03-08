1efeec77f20926070955d7f6814f71c879405396
MasuqaT <occar@hotmail.co.jp>
2018-03-08 21:13:03 +0900

    Configure Prettier.

--- Web/React/react-typescript-toolchain-2018-spring/src/App.tsx
+++ Web/React/react-typescript-toolchain-2018-spring/src/App.tsx
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
