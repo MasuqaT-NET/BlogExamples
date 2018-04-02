ef1ef99b1cee4ebb62b7dcd8705e6190962aa68a
MasuqaT <occar@hotmail.co.jp>
2018-04-02 21:47:33 +0900

    Implement server side.

---
+++ Web/gRPC/python_unittest/server/server_main.py
@@ -0,0 +1,25 @@
+import time
+from concurrent import futures
+
+import grpc
+
+import calculator_pb2_grpc
+from calculator_service import CalculatorService
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    calculator_pb2_grpc.add_CalculatorServicer_to_server(CalculatorService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
