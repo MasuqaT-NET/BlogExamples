6199f2cfde22a0056ade4fa1a4779fd191d8e54d
MasuqaT <occar@hotmail.co.jp>
2018-05-11 23:22:25 +0900

    Configure Webpack HMR.

--- Web/Misc/closure-library-webpack-hmr/src/js/main.js
+++ Web/Misc/closure-library-webpack-hmr/src/js/main.js
@@ -1,12 +1,26 @@
 goog.require("tutorial.notepad");
 
-function main() {
-  var noteData = [
-    {'title': 'Note 1', 'content': 'Content of Note 1'},
-    {'title': 'Note 2', 'content': 'Content of Note 2'}];
+var noteData = [
+  {'title': 'Note 1', 'content': 'Content of Note 1'},
+  {'title': 'Note 2', 'content': 'Content of Note 2'}];
 
+function main() {
   var noteListElement = document.getElementById('notes');
   var notes = tutorial.notepad.makeNotes(noteData, noteListElement);
 }
 
 main();
+
+if (module.hot) {
+  module.hot.accept();
+  if (module.hot.data) {
+    noteData = module.hot.data.noteData;
+  }
+  module.hot.dispose(function (data) {
+    var elem = document.getElementById('notes');
+    while (elem.firstChild) {
+      elem.removeChild(elem.firstChild);
+    }
+    data.noteData = noteData;
+  });
+}
--- Web/Misc/closure-library-webpack-hmr/webpack.config.dev.js
+++ Web/Misc/closure-library-webpack-hmr/webpack.config.dev.js
@@ -1,10 +1,69 @@
+const path = require("path");
+const webpack = require('webpack');
+
 module.exports = {
   mode: "development",
   resolve: {
-    extensions: [
-      '.mjs',
-      '.js',
-    ]
+    extensions: [".mjs", ".js"]
+  },
+  entry: {
+    bundle: [path.resolve(__dirname, "src/js/main.js")]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "demo")
   },
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /src.*\.js$/,
+        exclude: /node_modules/,
+        loader: 'closure-loader',
+        options: {
+          paths: [
+            path.resolve(__dirname, 'src'),
+            path.resolve(__dirname, 'node_modules/closure-builder/third_party/closure-library/closure/goog'),
+          ],
+          es6mode: true,
+          watch: true,
+          fileExt: '.js',
+        }
+      },
+      {
+        test: /node_modules.closure-builder.third_party.closure-library.closure.goog.*\.js$/,
+        loader: 'closure-loader',
+        options: {
+          paths: [
+            path.resolve(__dirname, 'node_modules/closure-builder/third_party/closure-library/closure/goog'),
+          ],
+          es6mode: true,
+          watch: true,
+          fileExt: '.js',
+        },
+        exclude: /node_modules.closure-builder.third_party.closure-library.closure.goog.base\.js$/,
+      },
+      {
+        test: /node_modules.closure-builder.third_party.closure-library.closure.goog.base\.js/,
+        use: [
+          'imports-loader?this=>{goog:{}}&goog=>this.goog',
+          'exports-loader?goog',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      goog: 'closure-builder/third_party/closure-library/closure/goog/base'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  optimization: {
+    namedModules: true
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "demo"),
+    port: 9000,
+    hot: true
+  }
 };
